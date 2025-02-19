47d5f88be79e8a4639088ea121edfd5b3cc80d54
Bipul Islam <bipulhstu@gmail.com>
2025-02-19 16:13:35 +0600

    Initial Commit

---
+++ StateVsObservedObject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StateVsObservedObject
+//
+//  Created by Bipul Islam on 19/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StateVsObservedObject/StateVsObservedObjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateVsObservedObjectApp.swift
+//  StateVsObservedObject
+//
+//  Created by Bipul Islam on 19/2/25.
+//
+
+import SwiftUI
+
+@main
+struct StateVsObservedObjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
